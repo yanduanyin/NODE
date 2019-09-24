c12546401c689879830c29c0880f202788c9ab34
yanduanyin <1097720997@qq.com>
2019-09-24 18:57:26 +0800

    spr

---
+++ spr/index.js
@@ -0,0 +1,12 @@
+// 前后端一致性
+console.log("hello world");
+console.log(new Date());
+console.log(Math);
+
+console.log(setTimeout);
+console.log(setInterval);
+// console.log(requestAnimationFrame);   //前端才有，后端不支持的requestAnimationFrame定时器
+
+console.log(__dirname); // __dirname  返回文件的相对路径
+console.log(__filename);// __filename 返回文件的物理路径
+console.log(process); // 为我们服务  传参？
